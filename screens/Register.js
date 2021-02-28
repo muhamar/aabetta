0fba8a83e99492ac39e03cf343fbe4a7b733c8be
dede ariansya <dedeariansya1@gmail.com>
2021-02-28 18:26:02 +0700

    update register page

--- screens/Register.js
+++ screens/Register.js
@@ -6,9 +6,9 @@ import {
   StatusBar,
   KeyboardAvoidingView
 } from "react-native";
-import { Block, Checkbox, Text, theme } from "galio-framework";
+import { Block, Text } from "galio-framework";
 
-import { Button, Icon, Input } from "../components";
+import { Button, Input } from "../components";
 import { Images, argonTheme } from "../constants";
 
 const { width, height } = Dimensions.get("screen");
@@ -18,137 +18,41 @@ class Register extends React.Component {
     return (
       <Block flex middle>
         <StatusBar hidden />
-        <ImageBackground
-          source={Images.RegisterBackground}
-          style={{ width, height, zIndex: 1 }}
-        >
+        <ImageBackground source={Images.RegisterBackground} style={{ width, height, zIndex: 1 }}>
           <Block flex middle>
             <Block style={styles.registerContainer}>
-              <Block flex={0.25} middle style={styles.socialConnect}>
-                <Text color="#8898AA" size={12}>
-                  Sign up with
-                </Text>
-                <Block row style={{ marginTop: theme.SIZES.BASE }}>
-                  <Button style={{ ...styles.socialButtons, marginRight: 30 }}>
-                    <Block row>
-                      <Icon
-                        name="logo-github"
-                        family="Ionicon"
-                        size={14}
-                        color={"black"}
-                        style={{ marginTop: 2, marginRight: 5 }}
-                      />
-                      <Text style={styles.socialTextButtons}>GITHUB</Text>
-                    </Block>
-                  </Button>
-                  <Button style={styles.socialButtons}>
-                    <Block row>
-                      <Icon
-                        name="logo-google"
-                        family="Ionicon"
-                        size={14}
-                        color={"black"}
-                        style={{ marginTop: 2, marginRight: 5 }}
-                      />
-                      <Text style={styles.socialTextButtons}>GOOGLE</Text>
-                    </Block>
-                  </Button>
-                </Block>
-              </Block>
               <Block flex>
-                <Block flex={0.17} middle>
-                  <Text color="#8898AA" size={12}>
-                    Or sign up the classic way
+                <Block flex={0.17} middle style={{ backgroundColor: '#fff', marginBottom: 20, paddingVertical: 30 }}>
+                  <Text color="#8898AA" size={32} bold>
+                    BUAT AKUN
                   </Text>
                 </Block>
                 <Block flex center>
-                  <KeyboardAvoidingView
-                    style={{ flex: 1 }}
-                    behavior="padding"
-                    enabled
-                  >
-                    <Block width={width * 0.8} style={{ marginBottom: 15 }}>
-                      <Input
-                        borderless
-                        placeholder="Name"
-                        iconContent={
-                          <Icon
-                            size={16}
-                            color={argonTheme.COLORS.ICON}
-                            name="hat-3"
-                            family="ArgonExtra"
-                            style={styles.inputIcons}
-                          />
-                        }
-                      />
+                  <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled>
+                    <Block middle style={{ marginBottom: 15 }}>
+                      <Input borderless placeholder="Name" iconContent={false} />
                     </Block>
-                    <Block width={width * 0.8} style={{ marginBottom: 15 }}>
-                      <Input
-                        borderless
-                        placeholder="Email"
-                        iconContent={
-                          <Icon
-                            size={16}
-                            color={argonTheme.COLORS.ICON}
-                            name="ic_mail_24px"
-                            family="ArgonExtra"
-                            style={styles.inputIcons}
-                          />
-                        }
-                      />
-                    </Block>
-                    <Block width={width * 0.8}>
-                      <Input
-                        password
-                        borderless
-                        placeholder="Password"
-                        iconContent={
-                          <Icon
-                            size={16}
-                            color={argonTheme.COLORS.ICON}
-                            name="padlock-unlocked"
-                            family="ArgonExtra"
-                            style={styles.inputIcons}
-                          />
-                        }
-                      />
-                      <Block row style={styles.passwordCheck}>
-                        <Text size={12} color={argonTheme.COLORS.MUTED}>
-                          password strength:
-                        </Text>
-                        <Text bold size={12} color={argonTheme.COLORS.SUCCESS}>
-                          {" "}
-                          strong
-                        </Text>
-                      </Block>
-                    </Block>
-                    <Block row width={width * 0.75}>
-                      <Checkbox
-                        checkboxStyle={{
-                          borderWidth: 3
-                        }}
-                        color={argonTheme.COLORS.PRIMARY}
-                        label="I agree with the"
-                      />
-                      <Button
-                        style={{ width: 100 }}
-                        color="transparent"
-                        textStyle={{
-                          color: argonTheme.COLORS.PRIMARY,
-                          fontSize: 14
-                        }}
-                      >
-                        Privacy Policy
-                      </Button>
+                    <Block middle style={{ marginBottom: 15 }}>
+                      <Input borderless placeholder="Username" iconContent={false} />
                     </Block>
                     <Block middle>
-                      <Button color="primary" style={styles.createButton}>
+                      <Input password borderless placeholder="Password" iconContent={false} />
+                    </Block>
+                    <Block middle>
+                      <Button color="primary" style={styles.mainButton}>
                         <Text bold size={14} color={argonTheme.COLORS.WHITE}>
-                          CREATE ACCOUNT
+                          BUAT AKUN
                         </Text>
                       </Button>
                     </Block>
                   </KeyboardAvoidingView>
+                </Block>
+                <Block flex={0.17} middle style={{ backgroundColor: '#fff', paddingBottom: 20 }}>
+                  <Button color="default" style={styles.secondButton}>
+                    <Text bold size={14} color={argonTheme.COLORS.WHITE}>
+                      LOGIN
+                    </Text>
+                  </Button>
                 </Block>
               </Block>
             </Block>
@@ -162,7 +66,7 @@ class Register extends React.Component {
 const styles = StyleSheet.create({
   registerContainer: {
     width: width * 0.9,
-    height: height * 0.78,
+    height: height * 0.6,
     backgroundColor: "#F4F5F7",
     borderRadius: 4,
     shadowColor: argonTheme.COLORS.BLACK,
@@ -206,8 +110,14 @@ const styles = StyleSheet.create({
     paddingTop: 13,
     paddingBottom: 30
   },
-  createButton: {
-    width: width * 0.5,
+  mainButton: {
+    width: width * 0.8,
+    marginHorizontal: 0,
+    marginTop: 25
+  },
+  secondButton: {
+    width: width * 0.8,
+    marginHorizontal: 0,
     marginTop: 25
   }
 });
